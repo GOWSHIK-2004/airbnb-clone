c6de4265f53e575723e3f851fc5efa275aef27b0
GOWSHIK-2004 <gowshikofficial2022@gmail.com>
2025-03-07 13:51:55 +0530

    Update dbConnection.js

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -4,9 +4,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+
+console.log("CONNECTION_STRING:", process.env.CONNECTION_STRING);
+
 const connectDb = asyncHandler(async () => {
+    if (!process.env.CONNECTION_STRING) {
+        throw new Error(" CONNECTION_STRING is undefined. Check your .env file.");
+    }
+
     const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-    console.log("database connected: ",connect.connection.host);
+    console.log("Database connected: ", connect.connection.host);
 });
 
 export default connectDb;
